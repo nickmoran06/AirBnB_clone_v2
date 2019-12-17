1c706dd20b4b4049579ac95c5dc0b6a95f065c5d
Juan Sebastian Calle <jusemonca@gmail.com>
2019-12-17 00:05:36 -0500

    [Feature]: update delete and all methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,18 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        my_dict = {}
+        objs = self.__objects
+        if cls:
+            for key, value in objs.items():
+                if isinstance(value, cls):
+                    my_dict[key] = value
+            return my_dict
         return self.__objects
 
     def new(self, obj):
@@ -55,3 +62,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Delete objs
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
+            self.save()
