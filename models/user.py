205f74176a054f2b783f1a86b9332cfaaf588bd7
nickmoran06 <956@holbertonschool.com>
2019-12-19 22:00:39 +0000

    [Update]: replace to double quotes

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-This is the user class
-"""
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -17,12 +16,12 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship('Place', cascade='all, delete, delete-orphan',
-                          backref='user')
-    reviews = relationship('Review', cascade='all, delete, delete-orphan',
-                           backref='user')
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
