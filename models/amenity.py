141c766d1241b4c63ef4584d5bc8c0bb73d747c0
Juan Sebastian Montealegre <jusemonca@gmail.com>
2019-12-20 01:07:15 +0000

    [Feature]: update amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,12 +2,19 @@
 """
 This is the amenity class
 """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+import os
 
-
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    data = os.getenv('HBNB_TYPE_STORAGE')
+    if data == 'db':
+        from models.place import place_amenity
+        place_amenities = relationship("Place", secondary=place_amenity)
