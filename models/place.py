ab4a26537f565e9e65b358ab74d609d057dc8e78
nickmoran06 <956@holbertonschool.com>
2019-12-20 19:23:30 +0000

    [Fix]: pep8

--- models/place.py
+++ models/place.py
@@ -44,7 +44,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
-    
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", cascade="all, delete, delete-orphan",
                                backref="place")
