26a894bc56b20e5ca683015bdd2ed6a027dcd49e
nickmoran06 <956@holbertonschool.com>
2020-01-10 22:38:16 +0000

    [Feature] Put the proper instructions to do a deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,36 +3,34 @@
 Fabric script that distributes an archive to your web servers, using the function do_deploy
 """
 from fabric.api import *
-from os import path
+import os
 
 env.hosts = ['35.231.153.28', '35.231.90.183']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
     """
     Function that distributes an archive to your web servers
     """
+    if os.path.isfile(archive_path) is False:
+        return False
+
+    file_tgz = archive_path.split("/")[-1]
+    name_file = file_tgz.replace(".tgz", "")
+    path = "/data/web_static/releases/" + name_file
 
     try:
-        file_tgz = archive_path.split('/')[-1]
-        name_file = file.replace(".tgz", "")
-        path = "/data/web_static/releases/" + name_file
-
-        if path.exists(archive_path) is False:
-            return False
-
-        put(archive_path, '/tmp/')
-        run("sudo mkdir -p {}".format(path))
-        run("sudo tar -xzf /tmp/{} -C {}".format(file_tgz, path))
+        put(archive_path, "/tmp/")
+        run("sudo mkdir {}/".format(path))
+        run("sudo tar -xzf /tmp/{} -C {}/".format(file_tgz, path))
         run("sudo rm /tmp/{}".format(file_tgz))
-        run("sudo mv {}/web_static/* {}".format(path, path))
+        run("sudo mv {}/web_static/* {}/".format(path, path))
         run("sudo rm -rf {}/web_static".format(path))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(path))
         print("New version deployed!")
 
         return True
-
-    except Exception:
-
+    except:
         return False
