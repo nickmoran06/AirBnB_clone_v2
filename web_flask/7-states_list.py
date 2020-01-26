09131cf8bba53d8fb8f331f378c21bff65e0b077
nickmoran06 <nicolasmoranpulido@gmail.com>
2020-01-26 23:45:08 +0000

    [Fix] Name of variables

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,18 +7,18 @@ from models import *
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def Cities_states():
-    """Method to display an specific HTML page"""
-    states_list = list(storage.all("State").values())
-    state = sorted(states_list, key=lambda s: s.name)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
 def Close_session(exception=None):
     """Teardown to close the session storage"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def Cities_states():
+    """Method to display an specific HTML page"""
+    states_list = list(storage.all("State").values())
+    states = sorted(states_list, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
